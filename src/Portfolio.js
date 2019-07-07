b626cd5e6ca049124875e0852923b9dd202fd67c
Todd Barchok <todd@barchok.com>
2019-07-07 11:34:38 -0400

    updated to actually work

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,72 +1,63 @@
 import React from 'react';
 import './Portfolio.css';
-import projects from './projects.js'
+import portfolioProjects from './projects.js'
 
 
-function Portfolio() {
-	function Tag(props)
-	{
-		return <li><button onClick={(e) => filterByTags(props.tag, e)}>{props.tag}</button></li>;
-	}
-	
-	function TagList(props)
-	{
-		const tags = props.tags;
-		return (
-			<ul className="tags">
-			{ tags.map((tag) =>
-				<Tag key={tag} tag={tag} />
-			)}
-			</ul>
-		);
-	}
-	
-	function Project(props)
-	{
-		let image;
-		if(props.project.img)
-		{
-			image = <img src="{props.project.img}" alt={props.project.alt}/>;
-		}
-		return (
-			<li>
-				<a href="{props.project.url}">{props.project.name}</a>
-				{image}
-				<p>{props.project.description}</p>
-				<TagList tags={props.project.tags} />
-			</li>
-		);
-	}
-	
-	class ProjectList extends React.Component {
-		constructor(props)
-		{
-			super(props);
-			const projects = props.projects;
-			this.filterByTags = this.filterByTags.bind(this);
-			
-			this.state = { listItems: projects.map((project) =>
-				<Project key={project.key}
-					project={project} />
-			)};
-		}
-		filterByTags(tag, e)
-		{
-			e.preventDefault();
-			console.log("filtering by " + tag);
-		}
-		render() {
-			return (
-			<ul>
-			{this.state.listItems}
-			</ul>
-			)
-		}
-	}
-
+function TagList(props)
+{
+	const tags = props.tags;
 	return (
-	  <ProjectList projects={projects} />
+		<ul className="tags">
+		{ tags.map((tag) => 
+			<li key={tag}><button onClick={(e) => props.filter(tag, e)}>{tag}</button></li>
+		)}
+		</ul>
 	);
+}
+
+function Project(props)
+{
+	let image;
+	if(props.project.img)
+	{
+		image = <img src="{props.project.img}" alt={props.project.alt}/>;
+	}
+	return (
+		<li>
+			<a href="{props.project.url}">{props.project.name}</a>
+			{image}
+			<p>{props.project.description}</p>
+			<TagList tags={props.project.tags} filter={props.filter} />
+		</li>
+	);
+}
+
+class Portfolio extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.filterByTags = this.filterByTags.bind(this);
+		this.state = { projects: portfolioProjects.slice() };
+	}
+	
+	filterByTags(tag, e)
+	{
+		e.preventDefault();
+		console.log("filtering by " + tag);
+	}
+	
+	render()
+	{
+		const listItems = this.state.projects.map((project) =>
+			<Project key={project.key} project={project} filter={this.filterByTags} />
+		);
+		return (
+		<ul>
+		{listItems}
+		</ul>
+		)
+	}
 }
 
 export default Portfolio;
